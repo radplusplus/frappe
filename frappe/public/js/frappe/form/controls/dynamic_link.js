e64d2addd1b30fa7694b8c4b231b33ce4bc2b943
René Mailloux <rene.mailloux@radplusplus.com>
2017-11-27 16:20:50 -0500

    V9 - 2

---
+++ frappe/public/js/frappe/form/controls/dynamic_link.js
@@ -0,0 +1,25 @@
+frappe.ui.form.ControlDynamicLink = frappe.ui.form.ControlLink.extend({
+	get_options: function() {
+		if(this.df.get_options) {
+			return this.df.get_options();
+		}
+		if (this.docname==null && cur_dialog) {
+			//for dialog box
+			return cur_dialog.get_value(this.df.options);
+		}
+		if (cur_frm==null && cur_list){
+			//for list page
+			let list_filter = cur_list.wrapper.find("input[data-fieldname*="+this.df.options+"]");
+			if(list_filter.length > 0) {
+				return list_filter.val();
+			}
+			return $(cur_page.page).find("input[data-fieldname*="+this.df.options+"]").val();
+		}
+		var options = frappe.model.get_value(this.df.parent, this.docname, this.df.options);
+		// if(!options) {
+		// 	frappe.msgprint(__("Please set {0} first",
+		// 		[frappe.meta.get_docfield(this.df.parent, this.df.options, this.docname).label]));
+		// }
+		return options;
+	},
+});
